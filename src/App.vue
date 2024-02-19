eab46692880344ba4387b2b79edb80e0f7a7e720
My Name <ChubarovR@lcoast.itstep>
2024-02-19 19:41:25 +0600

    end

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <section>
+      <button @click="prevSlide">left</button>
+      <div>
+        <SliderC :hData="hData" :textData="textData" :index="index" />
+      </div>
+      <button @click="nextSlide">right</button>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import SliderC from './sliderC.vue'
+
+const hData = ref(['aaaaaa', 'bbbbb', 'ssssss'])
+const textData = ref(['aaaaaa', 'bbbbb', 'ssssss'])
+let index = ref(0)
+
+const nextSlide = () => {
+  index.value = (index.value + 1) % hData.value.length
+}
+
+const prevSlide = () => {
+  index.value = (index.value - 1 + hData.value.length) % hData.value.length
+}
+</script>
+
+<style scoped>
+section {
+  display: flex;
+}
+</style>
---
+++ src/sliderC.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h3>{{ hData[index] }}</h3>
+      <p>
+        {{ textData[index] }}
+      </p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  
+  const props = defineProps<{
+    hData: string[],
+    textData: string[],
+    index: number
+  }>()
+  
+  </script>
+  
+  <style scoped></style>
+  
